0c24b6cb25b91f3ba490b7ea519e4b39e4d44ef4
VirtualRcoder <shubhamsh2820@gmail.com>
2021-01-21 14:33:35 +0530

    Created Boiler Plate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TODO from './components/Todo'
+
+function App() {
+  return (
+    <div className="App">
+      <TODO/>
+    </div>
+  );
+}
+
+export default App;
